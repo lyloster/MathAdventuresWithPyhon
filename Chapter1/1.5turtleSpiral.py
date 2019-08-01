e25d03e5ceb092450281b30cdc05321408476e3a
Kristina Ilyovska <26176057+lyloster@users.noreply.github.com>
2019-07-31 21:26:22 -0400

    1.5 done

--- Chapter1/1.5turtleSpiral.py
+++ Chapter1/1.5turtleSpiral.py
@@ -2,16 +2,14 @@ from turtle import *
 
 shape('turtle')
 
-def square(sidelength=5):
+def square(sidelength=100):
     for i in range(4):
-        forward(100)
+        forward(sidelength)
         right(90)
 
 
 speed(0)
 sidelength = 5
-
-#doesn't look like a spiral
 
 for i in range(60):
     square(sidelength)
